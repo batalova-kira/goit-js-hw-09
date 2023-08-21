349c63939a6097bf1dfd845f65d87b88f0ca6389
batalova-kira <128981298+batalova-kira@users.noreply.github.com>
2023-08-21 07:33:51 +0000

    Deploying to gh-pages from @ batalova-kira/goit-js-hw-09@e5cde5d19589535425b48ce5f227ad30fac6c4ed 🚀

--- 01-color-switcher.4ffe1518.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;t.addEventListener("click",(()=>{d=setInterval((()=>{t.disabled=!0,e.disabled=!1,document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.4ffe1518.js.map
