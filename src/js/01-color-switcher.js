e5cde5d19589535425b48ce5f227ad30fac6c4ed
batalova-kira <batalovakira@gmail.com>
2023-08-21 10:33:24 +0300

    task-1-6

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,8 @@
 import '../css/common.css';
 
+let timerId;
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
-let timerId = null;
 
 btnStart.addEventListener ("click", () => {
   timerId = setInterval(() => {
